c92120175662a95225b7d4ab818256b300648e44
TheGeeksAlive <info@thegeeksalive.com>
2018-08-28 19:09:27 +0530

    for loop 1

---
+++ for1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear;
+echo "enter a no:"
+read a
+for i in 1 2 3 4  5;
+do echo -e "You have entered : $a";
+done;
+
+
---
+++ for3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#With Step Size
+for i in {5..50..5}; do
+    echo "Welcome $i"
+done
